8487eb8e8648693ceb77caf03cd611a638884712
ksashik01 <complainsheet@gmail.com>
2023-06-21 19:58:54 +0600

    file

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGyqs6h1GuVAEkpMou6HjmR4I2rpdbA_k",
+  authDomain: "aircnc-client-site.firebaseapp.com",
+  projectId: "aircnc-client-site",
+  storageBucket: "aircnc-client-site.appspot.com",
+  messagingSenderId: "995222136138",
+  appId: "1:995222136138:web:fe72c068903bd20cd40db3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
