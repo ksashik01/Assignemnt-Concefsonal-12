5302ea1452714d95203302f38e7f67098d29e3c4
ksashik01 <kscomplainsheet@gmail.com>
2023-06-23 01:03:32 +0600

    file

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,6 +4,7 @@
   } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Login/Login";
   
 
   export const router = createBrowserRouter([
@@ -20,4 +21,11 @@ import Home from "../Pages/Home/Home/Home";
 
       ]
     },
+
+    {
+      path:'/login',
+      element:<Login></Login>
+    }
+
+
   ]);
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Router';
-import AuthProvider from './Providers/AuthProvider';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
