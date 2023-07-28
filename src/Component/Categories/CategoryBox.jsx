98761cccd9440015b5398480b25ae441c0115ca2
ksashik01 <complainsheet@gmail.com>
2023-07-28 17:48:30 +0600

    new items

--- src/Component/Categories/CategoryBox.jsx
+++ src/Component/Categories/CategoryBox.jsx
@@ -1,42 +1,58 @@
-import React from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import React from 'react'
 import qs from 'query-string'
 
-const CategoryBox = ({label, icon:Icon}) => {
-    const [params, setParams] = useSearchParams()
-    const navigate = useNavigate()
-    const handleClick = () =>{
-        let currentQuery = {}
-        if (params){
-            currentQuery = qs.parse(params.toString())
-        }
-        const updatedQuery ={
-            ...currentQuery,
-            categorey: label,
-        }  
+import { useNavigate, useSearchParams } from 'react-router-dom'
+const CategoryBox = ({ label, icon: Icon, selected }) => {
+  const [params, setParams] = useSearchParams()
 
-        const url = qs.stringifyUrl(
-            {
-              url: '/',
-              query: updatedQuery,
-            },
-            { skipNull: true }
-          )
-          navigate(url)
-
+  const navigate = useNavigate()
+  const handleClick = () => {
+    let currentQuery = {}
+    if (params) {
+      currentQuery = qs.parse(params.toString())
+    }
+    const updatedQuery = {
+      ...currentQuery,
+      category: label,
     }
 
-    return (
-        <div onClick={handleClick}  className='flex flex-col items-center cursor-pointer justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500'>
-
-
-            <Icon size={26}></Icon>
-
-            <div>{label}</div>
-            
-        </div>
+    const url = qs.stringifyUrl(
+      {
+        url: '/',
+        query: updatedQuery,
+      },
+      { skipNull: true }
     )
-    
-};
 
-export default CategoryBox;
+    navigate(url)
+  }
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${
+          selected
+            ? 'border-b-neutral-800 text-neutral-800'
+            : 'border-transparent text-neutral-500'
+        }
+     
+      `}
+    >
+      <Icon size={26} />
+      <div className='text-sm font-medium'>{label}</div>
+    </div>
+  )
+}
+
+export default CategoryBox
--- src/Component/Rooms/Rooms.jsx
+++ src/Component/Rooms/Rooms.jsx
@@ -1,66 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import Container from '../Container';
-import Card from './Card';
-import Loader from '../../Shared/Loader/Loader';
-import { useSearchParams } from 'react-router-dom';
 
 
+import Card from './Card'
+
+import { useSearchParams } from 'react-router-dom'
+
+import { useEffect, useState } from 'react'
+import Container from '../Container'
+import Heading from '../Heading/Heading'
+import Loader from '../../Shared/Loader/Loader'
 
 const Rooms = () => {
-      
-        const [params, setParams] = useSearchParams()
-        const category = params.get('category')
-        
+  const [params, setParams] = useSearchParams()
+  const category = params.get('category')
 
+  const [rooms, setRooms] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    fetch('./rooms.json')
+      .then(res => res.json())
+      .then(data => {
+        if (category) {
+          const filtered = data.filter(room => room.category === category)
+          setRooms(filtered)
+          console.log(filtered)
+        } else {
+          setRooms(data)
+        }
 
-        const [rooms, setRooms] =useState([])
-        const [loading, setLoading] = useState(false)
-   
+        setLoading(false)
+      })
+      .catch(err => console.log(err))
+  }, [category])
 
-        useEffect(() => {
-                setLoading(true)
-                fetch('rooms.json')
-                  .then(res => res.json())
-                  .then(data => {
-                    if (category) {
-                      const filtered = data.filter(room => room.category === category)
-                 
-                      setRooms(filtered)
-                    
-                    }
+  if (loading) {
+    return <Loader/>
+  }
+  return (
+    <Container>
+      {rooms && rooms.length > 0 ? (
+        <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          {rooms.map((room, index) => (
+            <Card key={index} room={room} />
+          ))}
+        </div>
+      ) : (
+        <div className='pt-12'>
+          <Heading
+            title='No Rooms Available In This Category!'
+            subtitle='Please Select Other Categories.'
+            center={true}
+          />
+        </div>
+      )}
+    </Container>
+  )
+}
 
-                    else{
-                        setRooms(data)
-                        console.log(data)
-                    }
-
-
-            
-                    setLoading(false)
-                  })
-                  .catch(err => console.log(err))
-              }, [category])
-            
-              if (loading) {
-                return <Loader />
-              }
-
-        return (
-                <Container>
-                      
-          {rooms && rooms.length > 0 ? <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-                {rooms.map((room,index) =>(<Card key={index} room={room}></Card>))}
-
-
-                        </div>:
-                        <div>
-                                
-                        </div>
-                        
-                        
-                        }
-                </Container>
-        );
-};
-
-export default Rooms;
+export default Rooms
